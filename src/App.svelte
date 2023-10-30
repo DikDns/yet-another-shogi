ec1b65db0dc6d30e6dd3a2e3cbef59285dca79f1
DikDns <dikdnssocial@gmail.com>
2023-10-30 14:19:58 +0700

    add Board instance

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,46 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  class Board {
+    coordinates: number[][];
+
+    constructor() {
+      this.coordinates = [];
+
+      for (let i = 9; i >= 1; i--) {
+        for (let j = "a".charCodeAt(0); j <= "i".charCodeAt(0); j++) {
+          this.coordinates.push([i, j]);
+        }
+      }
+    }
+  }
+
+  const board = new Board();
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="board-container">
+    {#each board.coordinates as coordinate (coordinate)}
+      <div class="board" />
+    {/each}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .board-container {
+    color: darkslategrey;
+    display: flex;
+    flex-wrap: wrap;
+    width: 500px;
+    height: 500px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  .board {
+    background-color: burlywood;
+    width: 11.1%;
+    height: 11.1%;
+    border: 1px solid black;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 </style>
